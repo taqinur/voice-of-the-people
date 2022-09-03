c8c4242316074fb88cff0f15941ed5363f8e6fe6
Taqinur <taqi1662@gmail.com>
2022-09-04 05:07:23 +0600

    Added News details

--- js/app.js
+++ js/app.js
@@ -12,34 +12,43 @@ const displayNewsCategories = allCategories =>{
         categoriesDiv.classList.add('btn-group');
         categoriesDiv.classList.add('mx-3');
         categoriesDiv.innerHTML=`
-        <button onclick="displayAllNews('${category.category_id}')" class="btn btn-primary" type="submit">${category.category_name}</button>
+        <button onclick="loadNews('${category.category_id}')" class="btn btn-primary" type="submit">${category.category_name}</button>
         `;
         categoriesContainer.appendChild(categoriesDiv);
     });
 }
 
-const loadNews = async() =>{
-    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
+const loadNews = async(category_id) =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data.category_id);
+    let viewers = data.data;
+    viewers.sort((a, b) => {
+        return b.total_view - a.total_view;
+        });
+    displayAllNews(data.data);
 }
 
 const displayAllNews = allNews =>{
     const newsContainer = document.getElementById('news-container');
-    allNews.forEach(news => {
+    newsContainer.innerHTML = '';
+    allNews.forEach(news =>{
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('card');
         newsDiv.innerHTML = `
         <div class="row g-0">
             <div class="col-md-4">
-                <img src="" class="img-fluid rounded-start" alt="...">
+                <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <h5 class="card-title">${news.title}</h5>
+                    <p class="card-text">${news.details}</p>
+                    <p class="card-text">
+                        <small class="text-start text-muted">
+                        <img src="${news.author.img}" class="rounded-circle" style="height: 50px;" alt="...">
+                        ${news.author.name}<br>${news.author.published_date}</small> <small class="text-center"><i class="fa-regular fa-eye"></i> ${news.total_view}</small>
+                    </p>
                 </div>
             </div>
         </div>
@@ -48,4 +57,4 @@ const displayAllNews = allNews =>{
     });
 }
 loadCategories();
-displayAllNews();
+loadNews();
