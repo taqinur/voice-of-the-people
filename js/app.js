77aceb1b0d807a3b3542a356c3992c4f33c7f74c
Taqinur <taqi1662@gmail.com>
2022-09-04 06:34:59 +0600

    Not found msg

--- js/app.js
+++ js/app.js
@@ -30,13 +30,24 @@ const loadNews = async(category_id) =>{
 }
 
 const displayAllNews = allNews =>{
+
+    const noNews = document.getElementById('not-found-msg');
+        if(allNews.length === 0){
+            noNews.classList.remove('d-none');
+        }
+        else{
+            noNews.classList.add('d-none');
+        }
+
     const newsContainer = document.getElementById('news-container');
     newsContainer.innerHTML = '';
     allNews.forEach(news =>{
+
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('card');
         let newsText = news.details;
-        newsText = newsText.slice(0,450);
+        newsText = newsText.slice(0,200);
+
         newsDiv.innerHTML = `
         <div class="row g-0">
             <div class="col-md-4">
@@ -45,11 +56,11 @@ const displayAllNews = allNews =>{
             <div class="col-md-8">
                 <div class="card-body">
                     <h5 class="card-title">${news.title}</h5>
-                    <p class="card-text">${newsText}</p>
+                    <p class="card-text">${newsText}...</p>
                     <p class="card-text">
                         <small class="text-start text-muted">
                         <img src="${news.author.img}" class="rounded-circle" style="height: 50px;" alt="...">
-                        ${news.author.name}<br>${news.author.published_date}</small> <small class="text-center"><i class="fa-regular fa-eye"></i> ${news.total_view}</small>
+                        ${news.author.name}<br>${news.author.published_date}</small> <span class="text-center"><i class="fa-regular fa-eye"></i> ${news.total_view}</span>
                     </p>
                 </div>
             </div>
